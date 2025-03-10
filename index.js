0ee80836112037ebc037d4f6b2ca7713d8431ce0
garima <saxenagarima2953@gmail.com>
2025-03-10 23:45:39 +0530

    improve logic when seconds and minutes input are greater than 60

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ let resetBtn = document.querySelector(".reset-btn");
 let countTimer = null;
 
 startBtn.addEventListener("click", () => {
-    if(hour.value == 0 && minute.value == 0 && second.value == 0){
+    if (hour.value == 0 && minute.value == 0 && second.value == 0) {
         alert("Please enter time");
         return;
     }
 
-    function startTimer(){
+    function startTimer() {
         startBtn.style.display = "none";
         pauseBtn.style.display = "initial";
         countTimer = setInterval(timerFunction, 1000);
@@ -32,54 +32,37 @@ resetBtn.addEventListener("click", () => {
     stopTimer();
 });
 
-function timerFunction () {
-    let currentSecond = parseInt(second.value);
-    let currentMinute = parseInt(minute.value);
-    let currentHour = parseInt(hour.value);
-
-    if(currentSecond > 60){
-        currentMinute = Math.floor(currentSecond / 60);
-        currentSecond -= 60;
+function timerFunction() {
+    if (second.value > 60) {
+        minute.value++;
+        second.value = parseInt(second.value) - 60;
     }
 
-    if(currentMinute > 60){
-        currentHour = Math.floor(currentMinute / 60);
-        currentMinute -= 60;
+    if (minute.value > 60) {
+        hour.value++;
+        minute.value = parseInt(minute.value) - 60;
     }
 
-    if(currentHour == 0 && currentMinute == 0 && currentSecond == 0){
+    minute.value = minute.value > 60 ? 60 : minute.value;
+
+    if (hour.value == 0 && minute.value == 0 && second.value == 0) {
         hour.value = "";
         minute.value = "";
         second.value = "";
         stopTimer();
-    }   
-    if (currentSecond === 0) {
-        if (currentMinute > 0) {
-            currentMinute--;
-            currentSecond = 59;
-        } else if (currentHour > 0) {
-            currentHour--;
-            currentMinute = 59;
-            currentSecond = 59;
-        }
-    } else {
-        currentSecond--;
     }
-
-    hour.value = currentHour < 10 ? `0${currentHour}` : currentHour;
-    minute.value = currentMinute < 10 ? `0${currentMinute}` : currentMinute;
-    second.value = currentSecond < 10 ? `0${currentSecond}` : currentSecond;
-
-    if(hour.value != 0 && minute.value == 0 && second.value == 0){
-        hour.value = parseInt(hour.value) - 1;
-        minute.value = 59;
+    else if (second.value != 0) {
+        second.value = `${second.value <= 10 ? "0" : ""}${second.value - 1}`;
+    }
+    else if (minute.value != 0 && second.value == 0) {
         second.value = 59;
+        minute.value = `${minute.value <= 10 ? "0" : ""}${minute.value - 1}`;
+    } 
+    else if (hour.value != 0 && minute.value == 0) {
+        minute.value = 60;
+        hour.value = `${hour.value <= 10 ? "0" : ""}${hour.value - 1}`;
     }
-
-    if(minute.value != 0 && second.value == 0){
-        minute.value = parseInt(minute.value) - 1;
-        second.value = 59;
-    }
+    return;
 }
 
 function stopTimer(value) {
